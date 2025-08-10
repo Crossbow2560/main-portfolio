f12a146787ed088df533aeb9f09f71c0ee993419
Crossbow2560 <nishit.bilimagga@gmail.com>
2025-08-10 19:55:36 +0530

    added animejs

---
+++ anime.js
@@ -0,0 +1,17 @@
+anime({
+	targets: '#title-name',
+	translateX: ['-100%', '0%'],
+	duration: 600,
+	easing: 'easeOutQuart',
+	delay: 500,
+	fill: 'forwards'
+});
+
+anime({
+	targets: '#desc',
+	translateX: ['-100%', '0%'],
+	duration: 700,
+	easing: 'easeOutQuart',
+	delay: 500,
+	fill: 'forwards'
+});
